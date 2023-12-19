b530add3ea6f1608687b12cb8f0192c7d048fb26
kun0404 <farismobile282@gmail.com>
2023-12-19 22:01:36 +0700

    first commit

---
+++ Js/javascript.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const slideImage = document.getElementById('slideImage');
+  let currentSlide = 0;
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % 2;
+    updateSlide();
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + 2) % 2;
+    updateSlide();
+  }
+
+  function updateSlide() {
+    slideImage.src = `../MEGALIB/Assets/slide${currentSlide + 1}.png`;
+  }
+
+  const nextBtn = document.getElementById('nextBtn');
+  const prevBtn = document.getElementById('prevBtn');
+
+  nextBtn.addEventListener('click', nextSlide);
+  prevBtn.addEventListener('click', prevSlide);
+
+  setInterval(nextSlide, 3200);
+});
