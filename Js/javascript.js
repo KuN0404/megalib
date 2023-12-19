600fe8bb23f179d8cf2204c9aedb874325812cbb
kun0404 <farismobile282@gmail.com>
2023-12-19 23:52:12 +0700

    ubah img 8

--- Js/javascript.js
+++ Js/javascript.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function updateSlide() {
-    slideImage.src = `/MEGALIB/Assets/slide${currentSlide + 1}.png`;
+    slideImage.src = `/MEGALIB/Assets/slide2.png`;
   }
 
   const nextBtn = document.getElementById('nextBtn');
